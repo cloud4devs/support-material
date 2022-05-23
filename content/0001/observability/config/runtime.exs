8b937eac90d33922da53dd8c85bf58a275816f80
Kadmo Hardy <kadmo@swap.financial>
2022-05-23 01:09:33 -0300

    chore: fix config file

--- content/0001/observability/config/runtime.exs
+++ content/0001/observability/config/runtime.exs
@@ -21,26 +21,28 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
-
   config :observability, Observability.Repo,
-    username: "postgres",
-    password: "postgres",
-    database: "observability_dev",
-    hostname: "localhost",
-    pool_size: 10
+    url: System.fetch_env!("DATABASE_URL"),
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base = "Ud7N82/OijPB2AJ/7K39kBwF5lzh6OaeHvoXEX07SsmdF3WRU265twSCjIDoMHP2"
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :observability, ObservabilityWeb.Endpoint,
     http: [
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: String.to_integer(System.get_env("PORT") || "4000")
+      port: port
     ],
     secret_key_base: secret_key_base
 
@@ -48,31 +50,13 @@ if config_env() == :prod do
     manual_metrics_start_delay: :no_delay,
     drop_metrics_group: [],
     grafana: [
-      host: "http://localhost:3000",
-      auth_token:
-        "eyJrIjoiTWRDM0N2eW1YNjNKSThDSXM5emE0c2s2OU1xVUJVcTMiLCJuIjoib2JzZXJ2YWJpbGl0eSIsImlkIjoxfQ==",
+      host: System.fetch_env!("GRAFANA_URL"),
+      auth_token: System.fetch_env!("GRAFANA_AUTH_TOKEN"),
       upload_dashboards_on_start: true,
-      folder_name: "observability",
+      folder_name: System.fetch_env!("GRAFANA_DASHBOARD_FOLDER_NAME"),
       annotate_app_lifecycle: true
     ],
-    prometheus_datasource_id: "Prometheus",
-    prometheus_default_selected_interval: "30s"
-
-  # ## Configuring the mailer
-  #
-  # In production you need to configure the mailer to use a different adapter.
-  # Also, you may need to configure the Swoosh API client of your choice if you
-  # are not using SMTP. Here is an example of the configuration:
-  #
-  #     config :observability, Observability.Mailer,
-  #       adapter: Swoosh.Adapters.Mailgun,
-  #       api_key: System.get_env("MAILGUN_API_KEY"),
-  #       domain: System.get_env("MAILGUN_DOMAIN")
-  #
-  # For this example you need include a HTTP client required by Swoosh API client.
-  # Swoosh supports Hackney and Finch out of the box:
-  #
-  #     config :swoosh, :api_client, Swoosh.ApiClient.Hackney
-  #
-  # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
+    prometheus_datasource_id: System.fetch_env!("PROMETHEUS_DATA_SOURCE_ID"),
+    prometheus_default_selected_interval:
+      System.fetch_env!("PROMETHEUS_DEFAULT_SELECTED_INTERVAL")
 end
