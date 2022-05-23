c567ac3a256f32b009faff8456905ffa86d3d484
Kadmo Hardy <kadmo@swap.financial>
2022-05-23 00:55:20 -0300

    chore: update endpoint config

--- content/0001/observability/config/runtime.exs
+++ content/0001/observability/config/runtime.exs
@@ -21,45 +21,26 @@ if System.get_env("PHX_SERVER") do
 end
 
 if config_env() == :prod do
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :observability, Observability.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    username: "postgres",
+    password: "postgres",
+    database: "observability_dev",
+    hostname: "localhost",
+    pool_size: 10
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
   # want to use a different value for prod and you most likely don't want
   # to check this value into version control, so we use an environment
   # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
-
-  host = System.get_env("PHX_HOST") || "example.com"
-  port = String.to_integer(System.get_env("PORT") || "4000")
+  secret_key_base = "Ud7N82/OijPB2AJ/7K39kBwF5lzh6OaeHvoXEX07SsmdF3WRU265twSCjIDoMHP2"
 
   config :observability, ObservabilityWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
+      port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
 
@@ -67,15 +48,15 @@ if config_env() == :prod do
     manual_metrics_start_delay: :no_delay,
     drop_metrics_group: [],
     grafana: [
-      host: System.fetch_env!("GRAFANA_URL"),
-      auth_token: System.fetch_env!("GRAFANA_AUTH_TOKEN"),
+      host: "http://localhost:3000",
+      auth_token:
+        "eyJrIjoiTWRDM0N2eW1YNjNKSThDSXM5emE0c2s2OU1xVUJVcTMiLCJuIjoib2JzZXJ2YWJpbGl0eSIsImlkIjoxfQ==",
       upload_dashboards_on_start: true,
-      folder_name: System.fetch_env!("GRAFANA_DASHBOARD_FOLDER_NAME"),
+      folder_name: "observability",
       annotate_app_lifecycle: true
     ],
-    prometheus_datasource_id: System.fetch_env!("PROMETHEUS_DATA_SOURCE_ID"),
-    prometheus_default_selected_interval:
-      System.fetch_env!("PROMETHEUS_DEFAULT_SELECTED_INTERVAL")
+    prometheus_datasource_id: "Prometheus",
+    prometheus_default_selected_interval: "30s"
 
   # ## Configuring the mailer
   #
